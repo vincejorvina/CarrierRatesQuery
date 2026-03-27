347ac6728afe46d19672719b353943f874ce4a8e
nibsy <nibsy@VINCE-DESKTOP>
2026-03-28 00:03:57 +0800

    initialize apphost

--- CarrierRatesQuery.AppHost/AppHost.cs
+++ CarrierRatesQuery.AppHost/AppHost.cs
@@ -1,3 +1,9 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var fedEx = builder.AddProject<Projects.CarrierRatesQuery_MockFedEx>("mockfedex");
+
+builder.AddProject<Projects.CarrierRatesQuery_Api>("api")
+    .WithReference(fedEx)
+    .WaitFor(fedEx);
+
 builder.Build().Run();
